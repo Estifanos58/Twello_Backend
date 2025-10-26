e4de67527d25d0c7f6f60049f990cc5882edef44
Estifanos58 <estifkebe08@gmail.com>
2025-10-26 10:13:47 +0300

    fix: update subscriptions schema to match original midDev schema

--- src/graphql/schemas/subscriptions.ts
+++ src/graphql/schemas/subscriptions.ts
@@ -1,9 +1,6 @@
 export const subscriptionsSchema = `#graphql
   type Subscription {
-    taskCreated(projectId: ID!): Task!
-    taskUpdated(projectId: ID!): Task!
-    taskDeleted(projectId: ID!): Task!
-    notificationReceived: Notification!
-    projectUpdated(workspaceId: ID!): Project!
+    taskStatusUpdated(workspaceId: ID!): Task!
+    notificationAdded(userId: ID!): Notification!
   }
 `;
